54a98e5c81946dfd6dddecc358b41bbd748ec662
ds9-waylab <111089633+ds9-waylab@users.noreply.github.com>
2023-06-21 14:14:28 -0700

    Update 00_t501_glio_folder_setup.py

--- 00_t501_glio_folder_setup.py
+++ 00_t501_glio_folder_setup.py
@@ -2,9 +2,9 @@ from google.colab import drive
 drive.mount('/content/drive')
 
 !mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma/imagesTr
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma/imagesTs
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma/labelsTr
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/imagesTr
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/imagesTs
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/labelsTr
 !mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_preprocessed
 !mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_results
