6b2161101a9c6e16fef1c94d9dedbfaf4fe11134
ds9-waylab <111089633+ds9-waylab@users.noreply.github.com>
2023-07-06 10:13:43 -0700

    Updated folder structure
    Updated to nnUNet v2

--- 01_t501_glio_data_setup.py
+++ 01_t501_glio_data_setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from google.colab import drive
 drive.mount('/content/drive')
 
@@ -28,7 +29,7 @@ len(train_segm_list)
 # Lets generate some commands to help us move the ground truth segmentation files to the right folder for nnU-Net training
 # They need to be in the labelsTr folder
 
-tr_segm_target_dir = '/content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma/labelsTr/'
+tr_segm_target_dir = '/content/drive/MyDrive/TCIA/nnUNet/nnUNet_raw/Dataset501_Glioblastoma/labelsTr/'
 
 for file in train_segm_list:
   tr_segm_file = file.split('_')[0]
@@ -45,10 +46,6 @@ for file in test_segm_list:
   just_id = int(patient_id) # Get the patient ID # and remove leading zeroes
   print ('!mv ' + segm_src_dir + file + ' ' + ts_segm_target_dir + just_id + '.nii.gz')
 
-# Let's get the training files into a list so we can iterate
-train_patient_list = os.listdir(tr_segm_target_dir)
-train_segm_list
-
 # nnU-Net requires image files to end in a sequence that looks like - "patientID_4 digit_MRI_sequence" number
 # For example, we need to replace the file name from UPENN-GBM-00001_11_T1.nii.gz to UPENN-GBM-00001_0000.nii.gz
 # Our Mapping - T1:0000, T1-GD:0001, T2:0002, FLAIR:0003
@@ -56,27 +53,10 @@ train_segm_list
 # We will only do this for patients in our training cohort (who have ground truth segmentation files)
 
 mri_img_src_dir = '/content/drive/MyDrive/TCIA/UPENN-GBM/images_structural/'
-tr_img_target_dir = '/content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma/imagesTr/'
+tr_img_target_dir = '/content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/imagesTr/'
 
 
 for patient in train_segm_list:
-  patient_id = patient.split('.')[0]
-  patient_dir = patient_id + '_11' # Get the patient list from our labelsTr folder and generate the directory name
-  patient_dir_path = mri_img_src_dir + patient_dir # Generate full path of the images source directory
-  just_id = int(patient_id.split('-')[2]) # Get the patient ID # and remove leading zeroes
-
-  # print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T1.nii.gz ' + tr_img_target_dir + str(just_id) + '_0000.nii.gz')
-  # print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T1GD.nii.gz ' + tr_img_target_dir + str(just_id) + '_0001.nii.gz')
-  # print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T2.nii.gz ' + tr_img_target_dir + str(just_id) + '_0002.nii.gz')
-  print ('!mv ' + patient_dir_path + '/' + patient_dir + '_FLAIR.nii.gz ' + tr_img_target_dir + str(just_id) + '_0003.nii.gz')
-
-# Now move all the test data files to the imagesTs directory
-
-mri_img_src_dir = '/content/drive/MyDrive/TCIA/UPENN-GBM/images_structural/'
-ts_img_target_dir = '/content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Task501_Glioblastoma/imagesTs/'
-
-
-for patient in test_segm_list:
   patient_id = patient.split('.')[0]
   patient_dir = patient_id + '_11' # Get the patient list from our labelsTr folder and generate the directory name
   patient_dir_path = mri_img_src_dir + patient_dir # Generate full path of the images source directory
@@ -86,3 +66,24 @@ for patient in test_segm_list:
   print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T1GD.nii.gz ' + tr_img_target_dir + str(just_id) + '_0001.nii.gz')
   print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T2.nii.gz ' + tr_img_target_dir + str(just_id) + '_0002.nii.gz')
   print ('!mv ' + patient_dir_path + '/' + patient_dir + '_FLAIR.nii.gz ' + tr_img_target_dir + str(just_id) + '_0003.nii.gz')
+
+# Now move all the test data files to the imagesTs directory
+
+mri_img_src_dir = '/content/drive/MyDrive/TCIA/UPENN-GBM/images_structural/'
+ts_img_target_dir = '/content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/imagesTs/'
+
+test_img_list = os.listdir(mri_img_src_dir)
+
+for patient in test_img_list:
+  patient_id = patient.split('.')[0]
+  #patient_dir = patient_id + '_11' # Get the patient list from our labelsTr folder and generate the directory name
+  patient_dir = patient_id
+  patient_dir_path = mri_img_src_dir + patient_dir # Generate full path of the images source directory
+  # just_id = int(patient_id.split('-')[2]) # Get the patient ID # and remove leading zeroes
+  temp_id = patient_id.split('-')[2]
+  just_id = int(temp_id.split('_')[0])
+
+  print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T1.nii.gz ' + ts_img_target_dir + str(just_id) + '_0000.nii.gz')
+  print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T1GD.nii.gz ' + ts_img_target_dir + str(just_id) + '_0001.nii.gz')
+  print ('!mv ' + patient_dir_path + '/' + patient_dir + '_T2.nii.gz ' + ts_img_target_dir + str(just_id) + '_0002.nii.gz')
+  print ('!mv ' + patient_dir_path + '/' + patient_dir + '_FLAIR.nii.gz ' + ts_img_target_dir + str(just_id) + '_0003.nii.gz')
