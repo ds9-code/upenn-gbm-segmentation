4f83562244a6d7189712900cab514b8c10471552
ds9-waylab <111089633+ds9-waylab@users.noreply.github.com>
2023-07-06 10:16:37 -0700

    Updated folder structure
    Updated to nnUNet v2 folder structure

--- 00_t501_glio_folder_setup.py
+++ 00_t501_glio_folder_setup.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from google.colab import drive
 drive.mount('/content/drive')
 
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/imagesTr
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/imagesTs
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_raw_data/Dataset501_Glioblastoma/labelsTr
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_preprocessed
-!mkdir -p /content/drive/MyDrive/TCIA/nnUNet_raw_data_base/nnUNet_results
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_raw
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_raw/Dataset501_Glioblastoma
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_raw/Dataset501_Glioblastoma/imagesTr
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_raw/Dataset501_Glioblastoma/imagesTs
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_raw/Dataset501_Glioblastoma/labelsTr
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_preprocessed
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_results
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_results/inference
+!mkdir -p /content/drive/MyDrive/TCIA/nnUNet/nnUNet_results/postprocessed
